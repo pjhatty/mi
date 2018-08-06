e2c92c9d6ce63076b97882816834ee26aac2dc4b
pjhatty <pjhattster@gmail.com>
2018-08-06 16:01:52 -0500

    made the prod version read env vars

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
+
+require('dotenv').config()
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FLICKR_API_KEY: JSON.stringify(process.env.FLICKR_API_KEY),
+  PHOTOSET_ID: JSON.stringify(process.env.PHOTOSET_ID),
+  FLICKR_USER_ID: JSON.stringify(process.env.FLICKR_USER_ID)
 }
