3a8a9067afc11c5e3ed9e2cbab439269fc83e96b
pjhatty <pjhattster@gmail.com>
2018-08-07 16:47:31 -0500

    done at work

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,12 @@ require('dotenv').config()
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DB_URL: JSON.stringify(process.env.FIREBASE_DB_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSSAGE_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSSAGE_SENDER_ID),
   FLICKR_API_KEY: JSON.stringify(process.env.FLICKR_API_KEY),
   PHOTOSET_ID: JSON.stringify(process.env.PHOTOSET_ID),
   FLICKR_USER_ID: JSON.stringify(process.env.FLICKR_USER_ID)
